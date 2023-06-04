f668da51acf918c319935669d0e4e76861e45dcd
Rafael Sarilho <rafaelsarilho@hotmail.com>
2023-06-03 22:45:44 -0300

    nova branch

--- lib/view/tela_inicio_grupos.dart
+++ lib/view/tela_inicio_grupos.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:mysquad/view/container_grupos.dart';
 
 class GruposInicio extends StatefulWidget {
   const GruposInicio({Key? key}) : super(key: key);
@@ -12,313 +13,42 @@ class GruposInicio extends StatefulWidget {
 class _GruposInicioState extends State<GruposInicio> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //Decoração do container principal
-      padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
-      margin: EdgeInsets.fromLTRB(20, 50, 20, 0),
-      decoration: BoxDecoration(
-          color: Color.fromRGBO(0, 37, 65, 1),
-          border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-          borderRadius: BorderRadius.circular(12)),
-      child: GridView.count(
-        crossAxisCount: 2, // Quantidade de colunas
-        mainAxisSpacing: 30, // Espaçamento vertical
-        crossAxisSpacing: 30, // Espaçamento horizontal
-        children: [
-          //CONTEUDO DA GRIDVIEW
-
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Grupo do Aramzin',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '4/5 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
+    return Stack(
+      children: [
+        Container(
+          //Decoração do container principal
+          padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+          margin: EdgeInsets.fromLTRB(20, 50, 20, 0),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(0, 37, 65, 1),
+            border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
+            borderRadius: BorderRadius.circular(12),
           ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Bora duo platina',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '1/2 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
+          child: GridView.count(
+            crossAxisCount: 2, // Quantidade de colunas
+            mainAxisSpacing: 30, // Espaçamento vertical
+            crossAxisSpacing: 30, // Espaçamento horizontal
+            children: [
+              //CONTEUDO DA GRIDVIEW
+              CustomContainer(
+                titulo: 'Adicionar novo grupo',
+                quantidade: '',
+                botao: 'Adicionar',
+                onPressed: () {},
+              ),
+            ],
           ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Orfãos do URF',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '3/5 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 20, 30, 5),
+          alignment: Alignment.bottomRight,
+          child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Color.fromRGBO(90, 252, 255, 0.7),
+            child: Icon(Icons.add),
           ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Builds conceito',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '2/5 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Só mulheres aqui',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '4/5 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Bora custom',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '7/10 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'A sigla LTBTQIA+',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '3/5 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Vem nordeste!',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '4/5 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Duo Ferro 3',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '1/2 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color.fromRGBO(54, 73, 84, 1),
-                border: Border.all(color: Color.fromRGBO(190, 252, 255, 0.5)),
-                borderRadius: BorderRadius.circular(10)),
-            child: Column(
-              children: [
-                SizedBox(height: 5),
-                Text(
-                  'Só Challenger',
-                  style: TextStyle(color: Colors.white, fontSize: 16),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  '2/5 players',
-                  style: TextStyle(color: Colors.white, fontSize: 11),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromRGBO(131, 128, 215, 1),
-                    ),
-                    child: Text('Entrar'))
-              ],
-            ),
-          ),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
